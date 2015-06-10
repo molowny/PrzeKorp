83ec962745dbcb287c9559ce8d7c127539a77ae5
Mariusz Ołownia <ollownia@gmail.com>
2015-06-10 09:55:10 +0200

    psql fix

--- import/queries/movies.sql
+++ import/queries/movies.sql
@@ -1,2 +1,2 @@
--- SELECT id, name FROM movies ORDER BY created_at
-SELECT id, name FROM movies where id IN (42, 43) ORDER BY created_at
+SELECT id, name FROM movies ORDER BY created_at
+-- SELECT id, name FROM movies where id IN (42, 43) ORDER BY created_at
